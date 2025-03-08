a0c81e67419a97b37ada78a61fc07215f26a35bb
Ariel Turchinsky <ariel7590@gmail.com>
2025-03-09 01:01:35 +0200

    initial commit

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+	title: "About",
+};
+
+export default function Page() {
+	return (
+		<div>
+			<h1>About the Wild Oasis</h1>
+		</div>
+	);
+}
---
+++ app/account/page.js
@@ -0,0 +1,9 @@
+export const metadata = {
+	title: "Guest Area",
+};
+
+export default function Page(){
+    return <div>
+        <h1>Account page</h1>
+    </div>
+}
