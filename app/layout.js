e5fa0c08e22c59cb5ea01569f9fd20189455d144
Ariel Turchinsky <ariel7590@gmail.com>
2025-03-10 23:23:24 +0200

    continued with this shit

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,32 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "./_components/navigation";
 import "@/app/_styles/globals.css";
 
-export const metadata={
-  // title: "The Wild Oasis"
-  title:{
-    template: "%s - The Wild Oasis",
-    default: "Welcome - The Wild Oasis",
-  },
-  description: "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beutiful mountains and dark forests"
-}
+import { Josefin_Sans } from "next/font/google";
+import Header from "@/app/_components/Header";
+
+const josefin = Josefin_Sans({
+	subsets: ["latin"],
+	display: "swap",
+});
+export const metadata = {
+	// title: "The Wild Oasis"
+	title: {
+		template: "%s - The Wild Oasis",
+		default: "Welcome - The Wild Oasis",
+	},
+	description:
+		"Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beutiful mountains and dark forests",
+};
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-        </header>
-				<Navigation />
-				<main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+			<body
+				className={`${josefin.className} antialiased  bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+			>
+				<Header />
+				<div className='flex-1 px-8 py-12 grid'>
+					<main className='max-w-7xl mx-auto w-full'>{children}</main>
+				</div>
 			</body>
 		</html>
 	);
