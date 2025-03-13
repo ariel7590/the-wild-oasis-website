706154cb595ed21f5a357c79d1f8eaa4646633e3
Ariel Turchinsky <ariel7590@gmail.com>
2025-03-13 22:56:00 +0200

    made all the functionality and server actions bullshit

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,17 +1,26 @@
 "use client";
 import React, { useState } from "react";
+import { UpdateGuest } from "../_lib/actions";
+import { useFormStatus } from "react-dom";
 
-export default function UpdateProfileForm({ children }) {
+export default function UpdateProfileForm({ guest, children }) {
 	const [count, setCount] = useState();
-	const countryFlag = "pt.jpg";
+	// const countryFlag = "pt.jpg";
+
+	const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
 	return (
 		<div>
-			<form className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'>
+			<form
+				action={UpdateGuest}
+				className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'
+			>
 				<div className='space-y-2'>
 					<label>Full name</label>
 					<input
 						disabled
+						defaultValue={fullName}
+						name='fullName'
 						className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
 					/>
 				</div>
@@ -20,6 +29,8 @@ export default function UpdateProfileForm({ children }) {
 					<label>Email address</label>
 					<input
 						disabled
+						defaultValue={email}
+						name='email'
 						className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
 					/>
 				</div>
@@ -39,17 +50,28 @@ export default function UpdateProfileForm({ children }) {
 				<div className='space-y-2'>
 					<label htmlFor='nationalID'>National ID number</label>
 					<input
+						defaultValue={nationalID}
 						name='nationalID'
 						className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm'
 					/>
 				</div>
 
 				<div className='flex justify-end items-center gap-6'>
-					<button className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'>
-						Update profile
-					</button>
+					<Button />
 				</div>
 			</form>
 		</div>
+	);
+}
+
+function Button() {
+	const { pending } = useFormStatus();
+	return (
+		<button
+			className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'
+			disabled={pending}
+		>
+			{pending ? "Updating..." : "Update profile"}
+		</button>
 	);
 }
--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,15 @@
+import {auth} from '@/app/_lib/auth'
+
 export const metadata = {
 	title: "Guest Area",
 };
 
-export default function Page(){
+
+export default async function Page(){
+  const session=await auth();
     return <div>
         <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome Ariel
+        Welcome {session.user.name.split(' ')[0]}
       </h2>
     </div>
 }
